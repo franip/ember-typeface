2d89919ed27f7c8ee8fa05dd9cd49d2db54d5a42
Jeff Jewiss <jeff@jeffjewiss.com>
2017-11-22 12:23:21 +0000

    Add Lato

--- index.js
+++ index.js
@@ -2,5 +2,70 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-typeface'
+  name: 'ember-typeface',
+
+  included() {
+    this.import('node_modules/typeface-lato/index.css');
+
+    this.import('node_modules/typeface-lato/files/lato-latin-100.woff', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-100.woff2', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-100italic.woff', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-100italic.woff2', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-300.woff', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-300.woff2', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-300italic.woff', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-300italic.woff2', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-400.woff', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-400.woff2', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-400italic.woff', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-400italic.woff2', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-700.woff', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-700.woff2', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-700italic.woff', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-700italic.woff2', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-900.woff', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-900.woff2', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-900italic.woff', {
+      destDir: 'assets/files'
+    });
+    this.import('node_modules/typeface-lato/files/lato-latin-900italic.woff2', {
+      destDir: 'assets/files'
+    });
+  }
 };
